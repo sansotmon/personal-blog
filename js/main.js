de5058414b9e5807ca91eb9aa7c8932741aaffa5
Santiago Soto <sansotmon@gmail.com>
2022-06-20 15:55:10 -0500

    added 21ss gaming startup translation

--- js/main.js
+++ js/main.js
@@ -42,6 +42,8 @@ function setTranslate() {
     $('.js-muvo').text(" "+language.muvo);
     $('.js-title-mimegafono').text(language.title_mimegafono);
     $('.js-mimegafono').text(" "+language.mimegafono);
+    $('.js-title-21ss').text(language.title_21ss);
+    $('.js-21ss').text(" "+language.ssgaming);
     
 }
 
